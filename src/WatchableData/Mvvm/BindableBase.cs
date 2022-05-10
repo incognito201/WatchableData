847692a05755b75317a2b0f2d54180ff12c3845d
Incógnito <incognitosenpai860@gmail.com>
2022-05-10 05:08:20 -0300

    Adição de classes bases para Mvvm

---
+++ src/WatchableData/Mvvm/BindableBase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WatchableData.Mvvm
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            PropertyChanged?.Invoke(this, args);
+        }
+    }
+}
---
+++ src/WatchableData/Mvvm/WatchableBase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace WatchableData.Mvvm
+{
+    public class WatchableBase : BindableBase, INotifyDataErrorInfo
+    {
+        private readonly Dictionary<string, List<string>> _errors = new Dictionary<string, List<string>>();
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (string.IsNullOrEmpty(propertyName))
+            {
+                return _errors.Values;
+            }
+
+            if (!_errors.ContainsKey(propertyName))
+            {
+                return Enumerable.Empty<List<string>>();
+            }
+
+            return _errors[propertyName];
+        }
+
+        public bool HasErrors
+        {
+            get => _errors.Count > 0;
+        }
+
+        protected void AddError(string error, [CallerMemberName] string propertyName = null)
+        {
+            RemoveErrors(propertyName);
+            AddErrors(new List<string> { error }, propertyName);
+        }
+
+        protected void AddErrors(List<string> error, [CallerMemberName] string propertyName = null)
+        {
+            _errors[propertyName] = error;
+            RaisePropertyErrorChanged(propertyName);
+        }
+
+        protected void RemoveErrors([CallerMemberName] string propertyName = "")
+        {
+            _errors.Remove(propertyName);
+            RaisePropertyErrorChanged(propertyName);
+        }
+
+        protected void RaisePropertyErrorChanged([CallerMemberName] string propertyName = null)
+        {
+            OnPropertyErrorChanged(new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyErrorChanged(DataErrorsChangedEventArgs args)
+        {
+            ErrorsChanged?.Invoke(this, args);
+        }
+    }
+}
