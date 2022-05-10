37f39bfdcaf32000c595aff05cbaec1c3e725932
Incógnito <incognitosenpai860@gmail.com>
2022-05-10 16:19:47 -0300

    Adição de projeto de amostra utilizando MahApps.Metro e Fody.

---
+++ src/WatchableData.Samples/WatchableData.Demo.Fody/OrderItem.cs
@@ -0,0 +1,30 @@
+﻿using WatchableData.Mvvm;
+using WatchableData.Validation;
+
+namespace WatchableData.Demo.Fody
+{
+    public class OrderItem : WatchableBase
+    {
+        public OrderItem()
+        {
+            var quantityDisposer = this.WhenPropertyChanged(x => x.Quantity)
+                .LessThan(this, 1)
+                .ApplyRule("Invalid quantity!");
+
+            var priceDisposer = this.WhenPropertyChanged(x => x.Price)
+                .LessThan<decimal>(this, 0)
+                .ApplyRule("Value cannot be negative!");
+
+            Disposer.Add(quantityDisposer);
+            Disposer.Add(priceDisposer);
+
+            Quantity = 1;
+        }
+
+        public string Item { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
